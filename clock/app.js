7a3a7870bcf7b07a9f38759fa5d0909a3a862840
jackfjo3 <jackfjo3@gmail.com>
2017-04-25 19:48:14 -0400

    remove with statment

--- clock/app.js
+++ clock/app.js
@@ -1,8 +1,8 @@
 function moveHands() {
-  with (new Date()) {
-    h = 30 * (getHours() % 12 + getMinutes() / 60); // 30 degrees for each hour
-    m = 6 * (getMinutes()); // 6 degrees each minute
-    s = 6 * (getSeconds()); 
+  var date = new Date(); // local variable date was defined rather than using the with statement
+    h = 30* date.getHours() % 12 + date.getMinutes() / 60; // 30 degrees for each hour
+    m = 6 * date.getMinutes(); // 6 degrees each minute
+    s = 6 * date.getSeconds(); 
     // now rotate the clock via changing css
 
     document.getElementById('hours').style.cssText = "-webkit-transform:rotate(" + h + "deg);";
@@ -10,6 +10,5 @@ function moveHands() {
     document.getElementById('seconds').style.cssText = "-webkit-transform:rotate(" + s + "deg);";
 
     setTimeout(moveHands, 1000);
-  }
 }
 window.onload = moveHands;
