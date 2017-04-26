313e24077c27805114153f4cf8e9b7f262d3cbb6
jackfjo3 <jackfjo3@gmail.com>
2017-04-26 17:53:11 -0400

    customize variable names

--- to_do/app.js
+++ to_do/app.js
@@ -1,12 +1,12 @@
 function newTask () {
-  var task = document.getElementById('input').value; // grabs what is inside input fiels
-  var ul = document.getElementById('list');
-  var li = document.createElement('li');
+  var task = document.getElementById('input').value; // grabs what is inside input field
+  var UL = document.getElementById('myList');  // sets the ul element to my UL variable
+  var LI = document.createElement('li'); // creates an li element that will recieve the task
 
-  li.appendChild(document.createTextNode("- "+ task));
-  ul.appendChild(li);
-  document.getElementById('input').value="";
-  li.onclick = removeItem;
+  LI.appendChild(document.createTextNode(task)); // appends to the created li element the task entered into the input field
+  UL.appendChild(LI); // appends to the list the li element that now holds the task from the input field
+  document.getElementById('input').value=""; // resets the input field to blank
+  LI.onclick = removeItem; // clicking an entered task will delete it ?? why don't we need () for this function?
 
   // program runs as enter key (keyCode 13) is released
 }
